0e60e4f3875f230c3d70b05b9250f427af1afcb4
Sam Grubb <samuel.grubb@softwire.com>
2020-01-12 15:31:03 +0000

    Change home page image position to bottom

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -22,7 +22,7 @@ export const IndexPageTemplate = ({
         backgroundImage: `url(${
           !!image.childImageSharp ? image.childImageSharp.fluid.src : image
         })`,
-        backgroundPosition: `top left`,
+        backgroundPosition: `bottom left`,
         backgroundAttachment: `fixed`,
       }}
     >
