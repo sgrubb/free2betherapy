aa9ea9f2a0db438397a8a4841ec64c64188cf1a4
Sam Grubb <samuel.grubb@softwire.com>
2020-01-12 23:17:10 +0000

    Update logo style

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -41,7 +41,7 @@ const Navbar = class extends React.Component {
       >
         <div className="container">
           <div className="navbar-brand">
-            <Link to="/" className="navbar-item" title="Logo">
+            <Link to="/" className="navbar-item logo" title="Logo">
               <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
             </Link>
             {/* Hamburger menu */}
